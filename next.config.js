68aa662aed089ffbd413825b7f32dedd3d6a7256
IsraBA <IsraelBenAri1000@gmail.com>
2024-07-17 15:07:29 +0300

    try to disable extentions

--- next.config.js
+++ next.config.js
@@ -1,39 +1,16 @@
-const runtimeCaching = require("next-pwa/cache");
-// const nextTranslate = require("next-translate");
-const nextTranslate = require("next-translate-plugin");
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
-  scope: "/",
-  sw: "service-worker.js",
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-module.exports = withPWA({
+// next.config.js ללא תוספים
+module.exports = {
   reactStrictMode: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   i18n: {
-    // These are all the locales you want to support in
-    // your application
     locales: ["en-US", "es", "fr", "nl-NL", "he"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "en-US",
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
     domains: [
       {
         domain: "example.com",
         defaultLocale: "en-US",
-        // other locales that should be handled on this domain
         locales: ["es"],
       },
       {
@@ -46,7 +23,6 @@ module.exports = withPWA({
       },
     ],
   },
-
   images: {
     domains: [
       "images.unsplash.com",
@@ -56,18 +32,7 @@ module.exports = withPWA({
       "fakestoreapi.com",
       "res.cloudinary.com",
       "lh3.googleusercontent.com",
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com",
-      "",
       "images.dashter.com",
     ],
   },
-
-  ...nextTranslate(),
-});
-
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-// module.exports = withBundleAnalyzer({});
+};
