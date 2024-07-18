d465e28d88b22ab695ae8f9fe17db0d7fcf15e9f
IsraBA <IsraelBenAri1000@gmail.com>
2024-07-18 16:25:20 +0300

    only hebrew anble

--- next.config.js
+++ next.config.js
@@ -22,28 +22,28 @@ module.exports = withPWA({
   i18n: {
     // These are all the locales you want to support in
     // your application 
-    locales: ["en-US", "es", "fr", "nl-NL", "he"],
+    locales: ["he"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "he",
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
-    domains: [
-      {
-        domain: "example.com",
-        defaultLocale: "en-US",
-        // other locales that should be handled on this domain
-        locales: ["es"],
-      },
-      {
-        domain: "example.nl",
-        defaultLocale: "nl-NL",
-      },
-      {
-        domain: "example.fr",
-        defaultLocale: "fr",
-      },
-    ],
+    // domains: [
+    //   {
+    //     domain: "example.com",
+    //     defaultLocale: "en-US",
+    //     // other locales that should be handled on this domain
+    //     locales: ["es"],
+    //   },
+    //   {
+    //     domain: "example.nl",
+    //     defaultLocale: "nl-NL",
+    //   },
+    //   {
+    //     domain: "example.fr",
+    //     defaultLocale: "fr",
+    //   },
+    // ],
   },
 
   images: {
