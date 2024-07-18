f59c11824111adb520a30f6dd6be3d83db2444ab
BizzStudio <office@bizzstudio.co.il>
2024-07-18 09:53:08 +0300

    Update InputShipping test .js

--- src/component/form/InputShipping.js
+++ src/component/form/InputShipping.js
@@ -1,10 +1,6 @@
 import DeliveryServices from "@services/DeliveryServices";
-import Cookies from "js-cookie";
-import useTranslation from "next-translate/useTranslation";
 import React from "react";
-import { FiInfo, FiTruck } from "react-icons/fi";
-import dayjs from 'dayjs';
-import 'dayjs/locale/he';
+import { FiTruck } from "react-icons/fi";
 
 const InputShipping = ({
   register,
@@ -15,53 +11,23 @@ const InputShipping = ({
   handleShippingCost,
   icon = <FiTruck />,
   isDeliverable,
-  note = '',
-  nextTime = null,
-  isDeliveryOpen = true,
 }) => {
 
-  const { t } = useTranslation();
-
-  let currentLang = Cookies.get('_lang');
-
-  switch (currentLang) {
-    case 'he':
-      currentLang = true;
-      break;
-    case 'en':
-      currentLang = false;
-      break;
-    default:
-      currentLang = false;
-      break;
-  }
-
-  const formatDate = (dateString) => {
-    dayjs.locale('he');
-    const date = dayjs(dateString);
-    return date.format('dddd, D בMMMM בשעה HH:mm');
-  };
-
   return (
-    <div className={`p-3 card border border-gray-200 ${isDeliverable && isDeliveryOpen ? "bg-white" : "opacity-60"} rounded-md h-full flex items-center`}>
-      <label className={`label w-full px-1.5 ${isDeliverable && isDeliveryOpen ? "cursor-pointer" : "cursor-not-allowed"}`}>
+    <div className={`p-3 card border border-gray-200 ${isDeliverable ? "bg-white" : "opacity-60"} rounded-md h-full flex items-center`}>
+      <label className={`label w-full px-1.5 ${isDeliverable ? "cursor-pointer" : "cursor-not-allowed"}`}>
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             <span className="text-3xl text-gray-400">
               {icon}
             </span>
             <div>
-              <h6 className={`flex items-center gap-1 font-serif font-medium text-base ${isDeliverable && isDeliveryOpen ? "text-gray-600" : "text-gray-400"} `}>
-                {value} {note && <span className="text-base text-gray-400" title={note}><FiInfo /></span>}
+              <h6 className={`font-serif font-medium text-base ${isDeliverable ? "text-gray-600" : "text-gray-400"} `}>
+                {value}
               </h6>
-              {!isDeliveryOpen ?
-                <p className={nextTime ? "text-sm text-red-500 -mt-1 pl-3" : "text-sm text-gray-400 -mt-1"}>
-                  {t("common:deliveriesNotAvailable")}
-                </p>
-                :
-                !isDeliverable && <p className={nextTime ? "text-sm text-red-500 -mt-1 pl-3" : "text-sm text-gray-400 -mt-1"}>
-                  {nextTime ? `${t("common:nextAvailable")} ${formatDate(nextTime)}` : t("common:cannotDeliver")}
-                </p>}
+              {!isDeliverable && <p className="text-sm text-gray-400 -mt-1">
+                אין משלוחים לכתובת שלך
+              </p>}
             </div>
           </div>
           <input
@@ -73,9 +39,9 @@ const InputShipping = ({
             })}
             name="shippingOption"
             type="radio"
-            disabled={!isDeliveryOpen || !isDeliverable}
+            disabled={!isDeliverable}
             value={value}
-            className={`form-radio outline-none focus:ring-0 text-customGreen bg-transparent cursor-pointer ${!isDeliverable || !isDeliveryOpen ? "cursor-not-allowed" : ""}`}
+            className={`form-radio outline-none focus:ring-0 text-customGreen bg-transparent cursor-pointer ${!isDeliverable && "cursor-not-allowed"}`}
           />
         </div>
       </label>
