c8b54c5f91655b2088ebd037f9cc2ff71b2a7543
bizzstudio <office@bizzstudio.co.il>
2024-07-20 10:48:22 +0300

    all src and public 2

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
 const runtimeCaching = require("next-pwa/cache");
-const nextTranslate = require('next-translate-plugin');
+const nextTranslate = require("next-translate-plugin");
 
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
+  buildExcludes: [/middleware-manifest\.json$/],
   scope: "/",
   sw: "service-worker.js",
   skipWaiting: true,
-  // disable: process.env.NODE_ENV === "development",
-  disable: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
-  typescript: { ignoreBuildErrors: true, },
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -24,44 +22,51 @@ module.exports = withPWA({
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["en-US", "es", "fr", "nl-NL", "he"],
-    // locales: ["he"],
+    locales: ["en-US", "es", "fr", "nl-NL","he"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "he",
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
-    // domains: [
-    //   {
-    //     domain: "example.com",
-    //     defaultLocale: "en-US",
-    //     // other locales that should be handled on this domain
-    //     locales: ["es"],
-    //   },
-    //   {
-    //     domain: "example.nl",
-    //     defaultLocale: "nl-NL",
-    //   },
-    //   {
-    //     domain: "example.fr",
-    //     defaultLocale: "fr",
-    //   },
-    // ],
+    domains: [
+      {
+        domain: "example.com",
+        defaultLocale: "he",
+        // other locales that should be handled on this domain
+        locales: ["he"],
+      },
+      {
+        domain: "example.en",
+        defaultLocale: "en-US",
+      },
+      {
+        domain: "example.fr",
+        defaultLocale: "fr",
+      },
+    ],
   },
 
+  // images: {
+  //   domains: [
+  //     "images.unsplash.com",
+  //     "img.icons8.com",
+  //     "i.ibb.co",
+  //     "i.postimg.cc",
+  //     "fakestoreapi.com",
+  //     "res.cloudinary.com",
+  //     "lh3.googleusercontent.com",
+  //     "res.cloudinary.com",
+  //     "lh3.googleusercontent.com",
+  //     "",
+  //     "images.dashter.com",
+  //   ],
+  // },
   images: {
-    domains: [
-      "images.unsplash.com",
-      "img.icons8.com",
-      "i.ibb.co",
-      "i.postimg.cc",
-      "fakestoreapi.com",
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com",
-      "",
-      "images.dashter.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 
