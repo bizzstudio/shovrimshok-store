22940ae2a29003786449d4ff27e97ccccc99904e
BizzStudio <office@bizzstudio.co.il>
2024-07-23 10:43:13 +0300

    Update OfferCard.js
    remove border

--- src/component/offer/OfferCard.js
+++ src/component/offer/OfferCard.js
@@ -40,7 +40,7 @@ const OfferCard = ({ discountProducts }) => {
 
   return (
     <div className="w-full group">
-      <div className="bg-gray-50  transition duration-150 ease-linear transform border-customGreen  shadow">
+      <div className="bg-gray-50  transition duration-150 ease-linear transform  shadow">
         <div className="bg-customBrown-light text-gray-900 px-6 py-2 rounded-t border-b flex items-center justify-center">
           <h3 className="text-base font-serif font-medium ">
             {showingTranslateValue(
