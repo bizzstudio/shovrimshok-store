1375063b74b2dfc44d0f9e0b93d27cc9b6bbce3a
BizzStudio <office@bizzstudio.co.il>
2024-07-23 11:38:25 +0300

    Update OfferCard.js
    rrr

--- src/component/offer/OfferCard.js
+++ src/component/offer/OfferCard.js
@@ -11,7 +11,7 @@ import Discount from "@component/common/Discount";
 
 const OfferCard = ({ discountProducts }) => {
   const { storeCustomizationSetting } = useGetSetting();
-  // const { showingTranslateValue } = useUtilsFunction();
+
 
   const { data: offers } = useAsync(() => OfferServices.getAllOffers());
 
@@ -43,9 +43,7 @@ const OfferCard = ({ discountProducts }) => {
       <div className="bg-gray-50 transition duration-150 ease-linear transform border-customGreen">
         <div className="bg-customBrown-light text-gray-900 px-6 py-2 rounded-t border-b flex items-center justify-center">
           <h3 className="text-base font-serif font-medium ">
-            // {showingTranslateValue(
-            //   storeCustomizationSetting?.home?.discount_title
-            // )}
+   
           </h3>
         </div>
         <div className="scroll-container">
