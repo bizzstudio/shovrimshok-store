3aff292e4cf0d1b1e40edc2ec258cb680fd70527
BizzStudio <office@bizzstudio.co.il>
2024-07-30 07:42:36 +0300

    install Hotjar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -49,6 +49,17 @@ class MyDocument extends Document {
               "https://meshek-kirshner.co.il/_next/static/media/newlogo.c452bf06.svg"
             }
           />
+            <!-- Hotjar Tracking Code for Site 5076708 (name missing) -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5076708,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
         </Head>
         <body>
           <Main />
