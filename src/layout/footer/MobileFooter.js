0197aa556f47d43185996574b5e7cc5a8126f2a1
IsraBA <IsraelBenAri1000@gmail.com>
2024-08-01 17:33:47 +0300

    add mobile category navbar

--- src/layout/footer/MobileFooter.js
+++ src/layout/footer/MobileFooter.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { useCart } from "react-use-cart";
-import { FiHome, FiUser, FiShoppingCart, FiAlignLeft,FiUserCheck } from "react-icons/fi";
+import { FiHome, FiUser, FiShoppingCart, FiAlignLeft, FiUserCheck } from "react-icons/fi";
 import { BsWhatsapp } from "react-icons/bs";
+import { TbCategory } from "react-icons/tb";
+import useTranslation from "next-translate/useTranslation";
 
 import { UserContext } from "@context/UserContext";
 import LoginModal from "@component/modal/LoginModal";
@@ -20,6 +22,8 @@ const MobileFooter = () => {
     state: { userInfo },
   } = useContext(UserContext);
 
+  const { t } = useTranslation();
+
   return (
     <>
       <LoginModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
@@ -30,11 +34,13 @@ const MobileFooter = () => {
         <button
           aria-label="Bar"
           onClick={toggleCategoryDrawer}
-          className="flex items-center justify-center flex-shrink-0 h-auto relative focus:outline-none"
+          className="flex flex-col items-center justify-center flex-shrink-0 h-auto relative focus:outline-none"
         >
           <span className="text-xl text-white">
-            <FiAlignLeft className="w-6 h-6 drop-shadow-xl" />
+            {/* <FiAlignLeft className="w-6 h-6 drop-shadow-xl" /> */}
+            <TbCategory className="w-6 h-6 drop-shadow-xl" />
           </span>
+          <p className="text-xs text-white text-center">{t("common:categories")}</p>
         </button>
         <Link
           href="/"
@@ -43,62 +49,67 @@ const MobileFooter = () => {
           aria-label="Home"
         >
           <FiHome className="w-6 h-6 drop-shadow-xl" />
+          <p className="text-xs text-white text-center">{t("common:home")}</p>
         </Link>
 
         <button
           onClick={toggleCartDrawer}
-          className="h-9 w-9 relative whitespace-nowrap inline-flex items-center justify-center text-white text-lg"
+          className="flex flex-col items-center justify-center"
         >
-          <span className="absolute z-10 top-0 right-0 inline-flex items-center justify-center p-1 h-5 w-5 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 bg-red-500 rounded-full">
-            {totalItems}
-          </span>
-          <FiShoppingCart className="w-6 h-6 drop-shadow-xl" />
+          <div className="relative whitespace-nowrap inline-flex items-center justify-center text-white text-lg">
+            <span className="absolute z-10 bottom-3 right-0 inline-flex items-center justify-center p-1 h-5 w-5 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 bg-red-500 rounded-full">
+              {totalItems}
+            </span>
+            <FiShoppingCart className="w-6 h-6 drop-shadow-xl" />
+          </div>
+          <p className="text-xs text-white text-center pl-1">{t("common:cart")}</p>
         </button>
         <button
           aria-label="User"
           type="button"
-          className="flex items-center text-xl text-customGreen bg-white indicator justify-center p-2 aspect-1 h-2/5 rounded-full outline outline-2 outline-white outline-offset-2"
+          className="flex flex-col items-center justify-center text-white"
         >
-          {userInfo?.image ? (
-            <Link href="/user/dashboard" className="relative top-1 w-6 h-6">
-              <Image
-                width={29}
-                height={29}
-                src={userInfo.image}
-                alt="user"
-                className="rounded-full"
-              />
-            </Link>
-          ) : userInfo?.name ? (
-            <Link
-              href="/user/dashboard"
-              className="leading-none font-bold font-serif block mb-0.5"
-            >
-               <FiUserCheck className="w-6 h-6 drop-shadow-xl" />
-            </Link>
+          <div
+          // className="flex flex-col gap-4 items-center justify-center text-xl text-customGreen bg-white p-0.5 
+          // aspect-1 rounded-full outline outline-2 outline-white outline-offset-2"
+          >
+            {userInfo?.image ? (
+              <Link href="/user/dashboard" className="w-6 h-6">
+                <Image
+                  width={29}
+                  height={29}
+                  src={userInfo.image}
+                  alt="user"
+                  className="rounded-full object-cover aspect-1 border-2 border-white"
+                />
+              </Link>
+            ) : userInfo?.name ? (
+              <Link
+                href="/user/dashboard"
+                className="leading-none font-bold font-serif block mb-0.5"
+              >
+                <FiUserCheck className="text-[26px] ml-1.5 drop-shadow-xl" />
+              </Link>
+            ) : (
+              <div onClick={() => setModalOpen(!modalOpen)}>
+                <FiUser className="text-[26px] drop-shadow-xl" />
+              </div>
+            )}
+          </div>
+          {userInfo?.name ? (
+            <p className="text-xs text-white text-center">{t("common:profile")}</p>
           ) : (
-            <span onClick={() => setModalOpen(!modalOpen)}>
-              <FiUser className="w-6 h-6 drop-shadow-xl" />
-            </span>
+            <p className="text-xs text-white text-center">{t("common:login")}</p>
           )}
         </button>
-        <button
-          aria-label="User"
-          type="button"
-          className="text-xl text-white indicator justify-center"
-        >
-   
-            <a
-              href="https://api.whatsapp.com/send/?phone=972525123003"
-              className="leading-none font-bold font-serif block"
-            >
-       
-            <span>
-              <BsWhatsapp className="w-6 h-6 drop-shadow-xl" />
-            </span>
-            </a>
-        
-        </button>
+        <a href="https://api.whatsapp.com/send/?phone=972525123003"
+          target="_blank"
+          className="flex flex-col gap-1 items-center justify-center text-xl text-white">
+          <span>
+            <BsWhatsapp className="w-6 h-6 drop-shadow-xl" />
+          </span>
+          <p className="text-xs text-white text-center">{t("common:contactUs")}</p>
+        </a>
       </footer>
     </>
   );
