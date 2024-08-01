7a37519c79cf6e69ffb7e41e93001fd4683e4c83
bizzstudio <office@bizzstudio.co.il>
2024-08-01 16:07:09 +0300

    גגגג

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -124,7 +124,7 @@ const Navbar = () => {
                 className="object-contain"
               />
             </Link>
-            <div className="w-full transition-all duration-200 ease-in-out lg:flex lg:max-w-[520px] xl:max-w-[750px] 2xl:max-w-[900px] md:mx-12 lg:mx-4 xl:mx-0">
+            <div className="w-full transition-all duration-200 dnone ease-in-out lg:flex lg:max-w-[520px] xl:max-w-[750px] 2xl:max-w-[900px] md:mx-12 lg:mx-4 xl:mx-0">
               <div className="w-full flex flex-col justify-center flex-shrink-0 relative z-30">
                 <div className="flex flex-col mx-auto w-full">
                   {/* <form
