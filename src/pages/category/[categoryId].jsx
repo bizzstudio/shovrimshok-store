145fa0f6bd7d36c68548ddab363f111ac91c7eb3
IsraBA <IsraelBenAri1000@gmail.com>
2024-09-04 15:31:17 +0300

    add category page

--- src/pages/category/[categoryId].jsx
+++ src/pages/category/[categoryId].jsx
@@ -5,40 +5,35 @@ import useTranslation from "next-translate/useTranslation";
 //internal import
 import Layout from "@layout/Layout";
 import useFilter from "@hooks/useFilter";
-import Card from "@component/cta-card/Card";
 import ProductServices from "@services/ProductServices";
 import ProductCard from "@component/product/ProductCard";
-import CategoryCarousel from "@component/carousel/CategoryCarousel";
 import { SidebarContext } from "@context/SidebarContext";
 import Loading from "@component/preloader/Loading";
-import AttributeServices from "@services/AttributeServices";
 import { useRouter } from "next/router";
 
 // import ctegories titles
-import fruitTitle from "public/titles/fruitTitle.svg"
-import herbsTitle from "public/titles/herbsTitle.svg"
-import leguTitle from "public/titles/leguTitle.svg"
-import ourOffers from "public/titles/ourOffers.svg"
-import vegTitle from "public/titles/vegTitle.svg"
-import honeyTitle from "public/titles/honeyTitle.svg"
-import sortProducts from "src/functions/sortProducts";
-import OfferServices from "@services/OfferServices";
+import fruitTitle from "public/titles/fruitTitle.svg";
+import herbsTitle from "public/titles/herbsTitle.svg";
+import leguTitle from "public/titles/leguTitle.svg";
+import ourOffers from "public/titles/ourOffers.svg";
+import vegTitle from "public/titles/vegTitle.svg";
+import honeyTitle from "public/titles/honeyTitle.svg";
 import useAsync from "@hooks/useAsync";
+import OfferServices from "@services/OfferServices";
+import AttributeServices from "@services/AttributeServices";
 
 const CategoryPage = ({ products, attributes }) => {
     const { t } = useTranslation();
     const { isLoading, setIsLoading } = useContext(SidebarContext);
     const [visibleProduct, setVisibleProduct] = useState(24);
-    const [isCategory, setIsCategory] = useState(false);
     const [category, setCategory] = useState('');
     const router = useRouter();
-    const { query } = router;
+    const { categoryId } = router.query; // מתייחס לקטגוריה ישירות מהנתב
     const { data: offers } = useAsync(() => OfferServices.getAllOffers());
 
     useEffect(() => {
-        if (query?.category) {
-            setIsCategory(true);
-            switch (query?.category) {
+        if (categoryId) {
+            switch (categoryId) { // משתמש ב-categoryId במקום query?.category
                 case 'פירות':
                     setCategory(fruitTitle.src);
                     break;
@@ -58,14 +53,11 @@ const CategoryPage = ({ products, attributes }) => {
                     setCategory(ourOffers.src);
                     break;
                 default:
-                    setIsCategory(false);
+                    setCategory('');
                     break;
             }
-        } else {
-            setIsCategory(false);
-            setCategory('');
         }
-    }, [query]);
+    }, [categoryId]);
 
     useEffect(() => {
         setIsLoading(false);
@@ -74,17 +66,11 @@ const CategoryPage = ({ products, attributes }) => {
     const { setSortedField, productData } = useFilter(products);
 
     return (
-        <Layout title="CategoryPage" description="This is search page">
+        <Layout title={`CategoryPage ${categoryId}`} description="This is category page">
             <div className="mx-auto max-w-screen-2xl px-3 sm:px-10">
                 <div className="flex py-5">
                     <div className="flex w-full">
                         <div className="w-full">
-                            {/* <div className="w-full grid grid-col gap-4 grid-cols-1 2xl:gap-6 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2">
-                <Card />
-              </div> */}
-                            {/* <div className="relative">
-                <CategoryCarousel />
-              </div> */}
                             {productData?.length === 0 ? (
                                 <div className="flex flex-col items-center text-center align-middle mx-auto p-5 my-5">
                                     <Image
@@ -99,32 +85,17 @@ const CategoryPage = ({ products, attributes }) => {
                                     </h2>
                                 </div>
                             ) : (
-                                isCategory ?
-                                    <img src={category} alt={query?.category} className="h-24 mx-auto animate-fadeIn" /> :
+                                category ? (
+                                    <img src={category} alt={categoryId} className="h-24 mx-auto animate-fadeIn" />
+                                ) : (
                                     <div className="flex justify-between my-3 bg-customBrown-light border border-gray-100 rounded p-3">
                                         <h6 className="text-sm font-serif">
                                             {t("common:totalI")}{" "}
                                             <span className="font-bold">{productData?.length}</span>{" "}
                                             {t("common:itemsFound")}
                                         </h6>
-                                        {/* מיון על פי מחיר, כרגע בהערה */}
-                                        {/* <span className="text-sm font-serif">
-                    <select
-                      onChange={(e) => setSortedField(e.target.value)}
-                      className="py-0 text-sm font-serif font-medium block w-full rounded border-0 bg-white pr-10 cursor-pointer focus:ring-0"
-                    >
-                      <option className="px-3" value="All" defaultValue hidden>
-                        {t("common:sortByPrice")}
-                      </option>
-                      <option className="px-3" value="Low">
-                        {t("common:lowToHigh")}
-                      </option>
-                      <option className="px-3" value="High">
-                        {t("common:highToLow")}
-                      </option>
-                    </select>
-                  </span> */}
                                     </div>
+                                )
                             )}
 
                             {isLoading ? (
@@ -132,18 +103,15 @@ const CategoryPage = ({ products, attributes }) => {
                             ) : (
                                 <>
                                     <div
-                                        // className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6 gap-2 md:gap-3 lg:gap-3"
-                                        className={`grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6 gap-2 md:gap-3 lg:gap-3 ${productData?.length < 6 ? 'justify-center' : ''
-                                            }`}
+                                        className={`grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6 gap-2 md:gap-3 lg:gap-3 ${productData?.length < 6 ? 'justify-center' : ''}`}
                                         style={{
                                             gridTemplateColumns: window.innerWidth < 640
-                                                ? `repeat(2, minmax(150px, 1fr))` // למסכים קטנים תמיד יהיה repeat של 2
+                                                ? `repeat(2, minmax(150px, 1fr))`
                                                 : productData?.length < 6
                                                     ? `repeat(${Math.min(productData?.length, 6)}, minmax(150px, 235px))`
                                                     : '',
                                         }}
                                     >
-
                                         {productData?.slice(0, visibleProduct).map((product, i) => (
                                             <ProductCard
                                                 key={i + 1}
@@ -157,7 +125,6 @@ const CategoryPage = ({ products, attributes }) => {
                                     {productData?.length > visibleProduct && (
                                         <button
                                             onClick={() => setVisibleProduct((pre) => pre + 36)}
-                                            // className="w-auto mx-auto md:text-sm leading-5 flex items-center transition ease-in-out duration-300 font-medium text-center justify-center border-0 border-transparent rounded-md focus-visible:outline-none focus:outline-none bg-indigo-100 text-gray-700 px-5 md:px-6 lg:px-8 py-2 md:py-3 lg:py-3 hover:text-white hover:bg-customGreen-dark h-12 mt-6 text-sm lg:text-sm "
                                             className="w-auto mx-auto mt-6 flex items-center gap-2 font-semibold cursor-pointer transition-all bg-customGreen text-white px-6 py-1.5 h-11 rounded-lg border-customGreen-dark border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
                                         >
                                             {t("common:loadMoreBtn")}
