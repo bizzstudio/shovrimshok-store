a2c23cac5cde2e612c4fb0561582c1e55b28880d
malky <m.0533121489@gmail.com>
2026-05-20 23:22:54 +0300

    tbhnmhv

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ import useUtilsFunction from "@hooks/useUtilsFunction";
 import CMSkeleton from "@component/preloader/CMSkeleton";
 import ourOffers from "public/titles/ourOffers.svg";
 import popolarTitle from "public/titles/popolarTitle.svg";
-import logoGif from "public/shovrimshok_loading.gif";
+import logoGif from "public/שוברים שוק-לוגו.p.gif";
 import Image from "next/image";
 import ShapiraTitle from "@component/shapira-title/ShapiraTitle";
 import MainBT from "@component/button/MainBT";
